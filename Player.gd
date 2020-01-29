26b2967b4a11e0a932becfdd9bb5fe3cff271981
Benjamin Anderson <heartbeast.studios@gmail.com>
2020-01-29 16:12:52 -0700

    Got the animation tree working and a simple state machine.

--- Player.gd
+++ Player.gd
@@ -1,46 +1,58 @@
 extends KinematicBody2D
 
+enum {
+	MOVE,
+	ATTACK
+}
+
 export(int) var MAX_SPEED = 60
 export(int) var ACCELERATION = 400
 export(float) var FRICTION = 0.4
 
 var velocity = Vector2.ZERO
+var state = MOVE
 
 onready var sprite = $Sprite
 onready var animationPlayer = $AnimationPlayer
-onready var runAnimationTree = $RunAnimationTree
-onready var idleAnimationTree = $IdleAnimationTree
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
-	var input_vector = Vector2.ZERO
-	input_vector.x =  Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	
-	if input_vector != Vector2.ZERO:
-		if input_vector.x == -1:
-			sprite.scale.x = -1
-		else:
-			sprite.scale.x = 1
-		idleAnimationTree.active = false
-		runAnimationTree.active = true
-		runAnimationTree.set("parameters/blend_position", input_vector)
-		idleAnimationTree.set("parameters/blend_position", input_vector)
-		input_vector = input_vector.normalized()
-		velocity += input_vector * ACCELERATION * delta
-		velocity = velocity.clamped(MAX_SPEED)
-	else:
-		idleAnimationTree.active = true
-		runAnimationTree.active = false
-		velocity = velocity.linear_interpolate(Vector2.ZERO, FRICTION)
-	
-	velocity = move_and_slide(velocity)
+	match state:
+		MOVE:
+			var input_vector = Vector2.ZERO
+			input_vector.x =  Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+			input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+			input_vector = input_vector.normalized()
+			
+			if input_vector != Vector2.ZERO:
+				animationState.travel("Run")
+				if input_vector.x < 0:
+					sprite.scale.x = -1
+				else:
+					sprite.scale.x = 1
+					
+				animationTree.set("parameters/Idle/blend_position", input_vector)
+				animationTree.set("parameters/Run/blend_position", input_vector)
+				animationTree.set("parameters/Attack/blend_position", input_vector)
+				
+				
+				velocity += input_vector * ACCELERATION * delta
+				velocity = velocity.clamped(MAX_SPEED)
+			else:
+				animationState.travel("Idle")
+				velocity = velocity.linear_interpolate(Vector2.ZERO, FRICTION)
+			
+			if Input.is_action_just_pressed("ui_accept"):
+				state = ATTACK
+				
+			
+			velocity = move_and_slide(velocity)
+		
+		ATTACK:
+			animationState.travel("Attack")
+			velocity = velocity.linear_interpolate(Vector2.ZERO, FRICTION)
+			velocity = move_and_slide(velocity)
 
-func get_direction_facing(angle):
-	if angle >= -PI/3 && angle <= PI/3:
-		return "Side"
-	elif angle < -PI/3 && angle > -PI +(PI/3):
-		return "Up"
-	elif angle > PI/3 && angle < PI - (PI/3):
-		return "Down"
-	else:
-		return "Side"
+func end_attack():
+	state = MOVE
