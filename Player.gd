ba0ae0a825fda679e6f81363a501800a476c4871
Benjamin Anderson <heartbeast.studios@gmail.com>
2020-02-04 17:18:23 -0700

    Broke something. Still figuring out the attack state/animation.

--- Player.gd
+++ Player.gd
@@ -13,14 +13,13 @@ var state = MOVE
 var velocity = Vector2.ZERO
 var input_vector = Vector2.ZERO
 
-
 onready var sprite = $Sprite
 onready var swordHitboxAxis = $SwordHitboxAxis
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
-func _physics_process(delta):
+func _process(delta):
 	match state:
 		MOVE:
 			update_input_vector()
@@ -43,28 +42,25 @@ func _physics_process(delta):
 			apply_friction()
 			move()
 
+func change_state_to(new_state):
+	state = new_state
 func update_input_vector():
 	input_vector.x =  Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
-
 func is_attempting_motion():
 	return (input_vector != Vector2.ZERO)
-
 func update_animation_to(animation_string):
 	animationState.travel(animation_string)
-
 func flip_the_sprite():
 	if input_vector.x < 0:
 		sprite.scale.x = -1
 	else:
 		sprite.scale.x = 1
-
 func update_animation_blend_positions():
 	animationTree.set("parameters/Idle/blend_position", input_vector)
 	animationTree.set("parameters/Run/blend_position", input_vector)
 	animationTree.set("parameters/Attack/blend_position", input_vector)
-
 func rotate_sword_hitbox():
 	if input_vector.x > 0:
 		swordHitboxAxis.rotation_degrees = 0
@@ -75,22 +71,16 @@ func rotate_sword_hitbox():
 			swordHitboxAxis.rotation_degrees = 90
 		else:
 			swordHitboxAxis.rotation_degrees = -90
-
 func apply_acceleration(delta):
 	velocity += input_vector * ACCELERATION * delta
-
 func limit_speed_by_max_speed():
 	velocity = velocity.clamped(MAX_SPEED)
-
 func confirm_attack():
 	if Input.is_action_just_pressed("ui_accept"):
 		state = ATTACK
-
 func apply_friction():
 	velocity = velocity.linear_interpolate(Vector2.ZERO, FRICTION)
-
 func move():
 	velocity = move_and_slide(velocity)
-
 func end_attack():
-	state = MOVE
+	change_state_to(MOVE)
